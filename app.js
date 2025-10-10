9b5f3d63cf4e416fd552566895846f66bf05a1e4
Tai Huy <23110023@student.hcmute.edu.vn>
2025-10-10 10:59:56 +0700

    Khởi tạo dự án Express + Handlebars
    - Thêm file app.js cấu hình Express server
    - Cài đặt view engine Handlebars
    - Thêm static files (CSS, logo)
    - Thêm layout và trang home
    - Tạo file .gitignore để loại trừ node_modules

---
+++ app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use('/static', express.static('static'));
+
+
+app.get('/', function (req, res) {
+    const categories = [
+        "AI - ChatGPT", "Lập trình - Web",
+        "Data Analysis", "Machine Learning",
+        "DevOps", "Cloud Computing",
+        "Cybersecurity", "Blockchain",
+        "IoT", "Mobile Development",
+        "Game Development"
+    ];
+    res.render('home', { categories });
+});
+
+
+app.listen(3000, function () {
+    console.log(`Server running at http://localhost:3000/`);
+});
